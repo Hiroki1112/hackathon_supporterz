716f4ddbed6860a81ab68c3b2c0d268cb1a49505
Hiroki1112 <pystdy@yahoo.co.jp>
2021-10-10 01:47:55 +0900

    投稿詳細ページ下部にユーザーカードを実装

---
+++ lib/screens/post_detail/cards/user_card.dart
@@ -0,0 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:hackathon_supporterz/models/user.dart';
+
+class UserCard extends StatefulWidget {
+  const UserCard({
+    Key? key,
+    required this.userId,
+  }) : super(key: key);
+  final String userId;
+
+  @override
+  _UserCardState createState() => _UserCardState();
+}
+
+class _UserCardState extends State<UserCard> {
+  var db = FirebaseFirestore.instance;
+  @override
+  Widget build(BuildContext context) {
+    var user =
+        db.collection('api').doc('v1').collection('user').doc(widget.userId);
+    return Card(
+      child: FutureBuilder(
+          future: user.get(),
+          builder: (BuildContext context,
+              AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              // ユーザーデータを受け取る
+              MyUser _user = MyUser();
+
+              if (snapshot.data!.data() == null) {
+                return const Text('データの取得に失敗しました');
+              }
+
+              _user.fromJson(snapshot.data!.data() ?? {});
+
+              return Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 7.0,
+                    ),
+                    child: const Icon(
+                      Icons.people,
+                    ),
+                  ),
+                  Text(
+                    _user.useName,
+                    style: const TextStyle(fontSize: 13),
+                  ),
+                ],
+              );
+            }
+            return const CircularProgressIndicator();
+          }),
+    );
+  }
+}
--- lib/screens/post_detail/post_detail_trend.dart
+++ lib/screens/post_detail/post_detail_trend.dart
@@ -3,12 +3,13 @@ import 'dart:convert';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:hackathon_supporterz/models/post.dart';
+import 'package:hackathon_supporterz/screens/post_detail/cards/body_card.dart';
+import 'package:hackathon_supporterz/screens/post_detail/cards/user_card.dart';
+import 'package:hackathon_supporterz/screens/post_detail/title/title.dart';
 import 'package:hackathon_supporterz/util/app_theme.dart';
 import 'package:hackathon_supporterz/util/config.dart';
 import 'package:hackathon_supporterz/widgets/appbar/my_appbar.dart';
-import 'package:hackathon_supporterz/widgets/tiles/post_tile.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-import 'package:markdown/markdown.dart' as md;
 
 class PostDetailTrend extends StatefulWidget {
   static String routeName = '/detail';
@@ -49,38 +50,9 @@ class _PostDetailTrendState extends State<PostDetailTrend> {
                 return SingleChildScrollView(
                   child: Column(
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 10, horizontal: 15),
-                        child: Text(_post.title),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          color: AppTheme.white,
-                          borderRadius: BorderRadius.circular(10),
-                          boxShadow: [
-                            BoxShadow(
-                              color: AppTheme.darkShadow,
-                              spreadRadius: 1.0,
-                              blurRadius: 3.0,
-                              offset: const Offset(1, 2),
-                            ),
-                          ],
-                        ),
-                        child: Container(
-                          // TextFormFirldは一行あたり25らしい
-                          height: MediaQuery.of(context).size.height * 0.75,
-                          padding: const EdgeInsets.all(10),
-                          child: WebView(
-                            javascriptMode: JavascriptMode.unrestricted,
-                            onWebViewCreated:
-                                (WebViewController webViewController) async {
-                              _controller = webViewController;
-                              await _loadHTML(_post.planText);
-                            },
-                          ),
-                        ),
-                      ),
+                      DetailTitle(title: _post.title),
+                      DetailBodyCard(bodyText: _post.bodyText),
+                      const UserCard(userId: 'userid'),
                     ],
                   ),
                 );
@@ -89,24 +61,6 @@ class _PostDetailTrendState extends State<PostDetailTrend> {
               return const CircularProgressIndicator();
             }),
       ),
-    );
-  }
-
-  Future _loadHTML(String rawText) async {
-    String previewHTML = '<body>' +
-        md.markdownToHtml(rawText.replaceAll('\\n', '\n')) +
-        '</body>';
-    // iOSの場合、メタタグを付けないとテキストが小さくなるため、
-    // Headerにメタタグを追加する
-    String header =
-        '<head><meta name="viewport" content="width=device-width, initial-scale=1.0"></head>';
-    previewHTML += header;
-    _controller.loadUrl(
-      Uri.dataFromString(
-        previewHTML,
-        mimeType: 'text/html',
-        encoding: Encoding.getByName('utf-8'),
-      ).toString(),
     );
   }
 }
