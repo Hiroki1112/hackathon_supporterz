12925184a97ae45b72c67ed98eaa86a0bead35b1
Hiroki1112 <pystdy@yahoo.co.jp>
2021-10-19 16:36:49 +0900

    ADD:delete functionの動作確認

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -6,9 +6,10 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-/// ユーザーの投稿があった際に他のドキュメントにコピーを作成する関数
-/// users/{documentId}/simplePosts, allPosts, tagsに書き込む
-exports.makeSimplePosts = functions.firestore.document('/api/v1/users/{userId}/posts/{postId}')
+// ユーザーの投稿があった際に他のドキュメントにコピーを作成する関数
+// users/{documentId}/simplePosts, allPosts, tagsに書き込む
+exports.makeSimplePosts = functions.firestore
+    .document("/api/v1/users/{userId}/posts/{postId}")
     .onCreate(async (snap, context) => {
     var _a;
     const v1CollectionRef = (_a = snap.ref.parent.parent) === null || _a === void 0 ? void 0 : _a.parent.parent;
@@ -22,14 +23,28 @@ exports.makeSimplePosts = functions.firestore.document('/api/v1/users/{userId}/p
         goodCount: snap.data().goodCount,
         timeCreated: snap.data().timeCreated,
         timeUpdated: snap.data().timeUpdated,
+        docId: snap.ref.id,
     };
-    /// 三箇所に書き込む
-    await (v1CollectionRef === null || v1CollectionRef === void 0 ? void 0 : v1CollectionRef.collection('allPosts').add(simplePost));
-    await snap.ref.collection('simplePosts').add(simplePost);
-    // You must return a Promise when performing asynchronous tasks inside a Functions such as
-    // writing to Firestore.
-    // Setting an 'uppercase' field in Firestore document returns a Promise.
-    // return snap.ref.set({ uppercase }, { merge: true });
+    // 三箇所に書き込む
+    await (v1CollectionRef === null || v1CollectionRef === void 0 ? void 0 : v1CollectionRef.collection("allPosts").add(simplePost));
+    await snap.ref.collection("simplePosts").add(simplePost);
+    return 0;
+});
+exports.deleteSimplePosts = functions.firestore
+    .document("/api/v1/users/{userId}/posts/{postId}")
+    .onDelete(async (snap, context) => {
+    var _a;
+    const v1CollectionRef = (_a = snap.ref.parent.parent) === null || _a === void 0 ? void 0 : _a.parent.parent;
+    // 関連するドキュメントを削除する
+    await (v1CollectionRef === null || v1CollectionRef === void 0 ? void 0 : v1CollectionRef.collection("allPosts").where("postId", "==", snap.id).get().then(async (result) => {
+        if (!result.empty) {
+            // 削除する
+            result.docs.forEach(async (value) => {
+                await (v1CollectionRef === null || v1CollectionRef === void 0 ? void 0 : v1CollectionRef.collection("allPosts").doc(value.id).delete());
+            });
+        }
+    }));
+    // await snap.ref.collection("simplePosts").add(simplePost);
     return 0;
 });
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,26 +11,49 @@ admin.initializeApp();
 
 // ユーザーの投稿があった際に他のドキュメントにコピーを作成する関数
 // users/{documentId}/simplePosts, allPosts, tagsに書き込む
-// eslint-disable-next-line max-len
-exports.makeSimplePosts = functions.firestore.document("/api/v1/users/{userId}/posts/{postId}").onCreate(async (snap, context) => {
-  const v1CollectionRef = snap.ref.parent.parent?.parent.parent;
+exports.makeSimplePosts = functions.region("asia-northeast1").firestore
+    .document("/api/v1/users/{userId}/posts/{postId}")
+    .onCreate(async (snap, context) => {
+      const v1CollectionRef = snap.ref.parent.parent?.parent.parent;
 
-  // 必要情報のみを取得
-  const simplePost: SimplePost = {
-    title: snap.data().title,
-    techTag: snap.data().techTag,
-    headerImageURL: snap.data().headerImageURL,
-    postId: snap.data().postId,
-    userId: snap.data().userId,
-    goodCount: snap.data().goodCount,
-    timeCreated: snap.data().timeCreated,
-    timeUpdated: snap.data().timeUpdated,
-  };
+      // 必要情報のみを取得
+      const simplePost: SimplePost = {
+        title: snap.data().title,
+        techTag: snap.data().techTag,
+        headerImageURL: snap.data().headerImageURL,
+        postId: snap.data().postId,
+        userId: snap.data().userId,
+        goodCount: snap.data().goodCount,
+        timeCreated: snap.data().timeCreated,
+        timeUpdated: snap.data().timeUpdated,
+        docId: snap.ref.id,
+      };
 
-  // 三箇所に書き込む
-  await v1CollectionRef?.collection("allPosts").add(simplePost);
-  await snap.ref.collection("simplePosts").add(simplePost);
-  return 0;
-});
+      // 三箇所に書き込む
+      await v1CollectionRef?.collection("allPosts").add(simplePost);
+      await snap.ref.collection("simplePosts").add(simplePost);
+      return 0;
+    });
 
+
+exports.deleteSimplePosts = functions.region("asia-northeast1").firestore
+    .document("/api/v1/users/{userId}/posts/{postId}")
+    .onDelete(async (snap, context) => {
+      const v1CollectionRef = snap.ref.parent.parent?.parent.parent;
+
+      // 関連するドキュメントを削除する
+      await v1CollectionRef?.collection("allPosts")
+            .where("postId", "==", snap.id).get()
+            .then(async (result)=>{
+              if (!result.empty) {
+                // 削除する
+                result.docs.forEach(async (value)=>{
+                  await v1CollectionRef?.collection("allPosts")
+                  .doc(value.id).delete();
+                });
+              }
+            });
+      // await snap.ref.collection("simplePosts").add(simplePost);
+      return 0;
+    });
 
